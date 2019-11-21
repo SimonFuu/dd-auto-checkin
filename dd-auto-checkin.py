4464b823ca85c4bd62f1bd760e0133cb0ce94fbd
Simon Fu <simon-fu@starlinke.com>
2019-11-21 10:43:50 +0800

    完成打卡

--- dd-auto-checkin.py
+++ dd-auto-checkin.py
@@ -7,6 +7,7 @@
 import configparser
 import datetime
 import json
+import os
 import random
 import subprocess
 import threading
@@ -32,7 +33,7 @@ def with_open_close_dd(func):
             process = subprocess.Popen(operation, shell=False, stdout=subprocess.PIPE)
             process.wait()
             time.sleep(5)
-        time.sleep(10)
+        time.sleep(20)
         print("打开打卡页面成功")
         # 包装函数
         func(self, *args, **kwargs)
@@ -50,22 +51,24 @@ class CheckIn:
         # 电源键
         adb_path = config.get('adb', 'path')
         unlock_towards = config.get('screen', 'unlock_towards')
-        screenH = int(config.get('screen', 'height'))
-        screenW = int(config.get('screen', 'width'))
+        screen_h = int(config.get('screen', 'height'))
+        screen_w = int(config.get('screen', 'width'))
         if unlock_towards == 'horizon':
             # 水平方向解锁
-            startX = screenW * 5 / 100
-            startY = screenH * 50 / 100
-            endX = screenW * 95 / 100
-            endY = screenH * 50 / 100
+            start_x = screen_w * 5 / 100
+            start_y = screen_h * 50 / 100
+            end_x = screen_w * 95 / 100
+            end_y = screen_h * 50 / 100
         else:
             # 竖直方向
-            startX = screenW * 50 / 100
-            startY = screenH * 60 / 100
-            endX = screenW * 50 / 100
-            endY = screenH * 90 / 100
-        self.adb_unlock = '%sadb shell input swipe %s %s %s %s' % (adb_path, startX, startY, endX, endY)
+            start_x = screen_w * 50 / 100
+            start_y = screen_h * 60 / 100
+            end_x = screen_w * 50 / 100
+            end_y = screen_h * 90 / 100
+        # 电源按钮
         self.adb_power_btn = '"%sadb" shell input keyevent 26' % adb_path
+        # 解锁屏幕
+        self.adb_unlock = '%sadb shell input swipe %s %s %s %s' % (adb_path, start_x, start_y, end_x, end_y)
         # 打开钉钉
         self.adb_open_dd = '%sadb shell monkey -p com.alibaba.android.rimet -c android.intent.category.LAUNCHER 1' % adb_path
         # 关闭钉钉
@@ -75,7 +78,8 @@ class CheckIn:
         # 删除截图
         self.adb_remove_screen_cap = '%sadb shell rm -r sdcard/screen.png' % adb_path
         # 拉取截图
-        self.adb_pull_screen_cap = '%sadb pull sdcard/screen.png %s' % (adb_path, config.get('screen', 'local_path'))
+        # self.adb_pull_screen_cap = '%sadb pull sdcard/screen.png %s' % (adb_path, config.get('screen', 'local_path'))
+        self.adb_pull_screen_cap = '%sadb pull sdcard/screen.png %s' % (adb_path, os.getcwd())
         # 点击work tab
         self.adb_tap_work_tab = '%sadb shell input tap %s' % (adb_path, config.get("screen", "work_tab"))
         # 点击签到 Function
@@ -90,14 +94,13 @@ class CheckIn:
     @with_open_close_dd
     def check(self, action):
         action_list = []
-        if action == 'checkin':
-            action_list.append(self.adb_tap_checkin)
-        else:
-            action_list.append(self.adb_tap_checkout)
+        # if action == 'checkin':
+        #     action_list.append(self.adb_tap_checkin)
+        # else:
+        #     action_list.append(self.adb_tap_checkout)
         action_list.append(self.adb_screen_cap)
         action_list.append(self.adb_pull_screen_cap)
         for item in action_list:
-            print(item)
             process = subprocess.Popen(item, shell=False, stdout=subprocess.PIPE)
             process.wait()
             time.sleep(5)
@@ -119,11 +122,20 @@ def is_weekend():
     return False
 
 
-def checkin_check(config, now):
-    date = now.strftime('%Y-%m-%d')
-    week_day = datetime.datetime.now().strftime("%w")
-    hours = now.strftime('%H:%M')
-    return True
+def time_check(now, conf):
+    now_time = now.strftime('%H:%M')
+    if now_time == '00:00' or now_time == '12:00':
+        global checked
+        checked = False
+    if checked:
+        return False
+    else:
+        if now_time == conf.get('check_time', 'checkin'):
+            return 'checkin'
+        elif now_time == conf.get('check_time', 'checkout'):
+            return 'checkout'
+        else:
+            return False
 
 
 def checkin_main():
@@ -132,23 +144,24 @@ def checkin_main():
     :return:
     """
     global config
-    global weekend
-    global checked_in
-    global checkinTime
-    global checked_out
-    global checkoutTime
+    global checked
+    global checking
     while 1:
         now = datetime.datetime.now()
-        if checkin_check(config, now):
+        check_action = time_check(now, config)
+        if check_action:
             # 触发DD打卡
-            delay = random_delay()
-            print("延迟 %s 后执行" % delay)
-            time.sleep(delay)
-            print("%s 开始执行打卡操作" % now.strftime('%Y-%m-%d %H:%M:%s'))
-            dd = CheckIn(config)
-        if now.strftime('%H:%M') == '00:00':
-            # 重置
-            pass
+            if not checking:
+                checking = True
+                delay = random_delay()
+                print("准备进行打卡从奥做，延迟 %s 后执行" % delay)
+                time.sleep(delay)
+                print("%s 开始执行打卡操作" % datetime.datetime.now())
+                dd = CheckIn(config)
+                dd.check(check_action)
+                print("%s 打卡完成" % datetime.datetime.now())
+                checked = True
+                checking = False
         time.sleep(0.5)
 
 
@@ -184,11 +197,6 @@ def service_main():
     :return:
     """
     global config
-    global weekend
-    global checked_in
-    global checkinTime
-    global checked_out
-    global checkoutTime
     global queue
     client = mqtt.Client(config.get('mqtt', 'uid'))
     client.username_pw_set(config.get('mqtt', 'username'), config.get('mqtt', 'password'))
@@ -201,24 +209,22 @@ def service_main():
 
 
 def main():
-    # checkin_thread = threading.Thread(target=checkin_main, name='CheckinThread')
+    print('开始运行，准备启动子线程')
+    checkin_thread = threading.Thread(target=checkin_main, name='CheckinThread')
     service_thread = threading.Thread(target=service_main, name='ServiceThread')
     manual_check_thread = threading.Thread(target=manual_check, name='ManualCheckThread')
-    # checkin_thread.start()
+    checkin_thread.start()
     service_thread.start()
     manual_check_thread.start()
-    # checkin_thread.join()
+    checkin_thread.join()
     service_thread.join()
     manual_check_thread.join()
 
 
 if __name__ == '__main__':
-    weekend = False
-    checked_in = False
-    checkinTime = '08:45'
-    checked_out = True
-    checkoutTime = '18:20'
+    checked = False
+    checking = False
     queue = Queue(1024)
     config = configparser.ConfigParser(allow_no_value=False)
-    config.read("config.cfg")
+    config.read(os.path.dirname(__file__).replace('\\', '/') + "/config.cfg")
     main()
